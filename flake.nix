384459710923348a7d26d4c8475fbd1b52436bd9
Alessandro <dalfovo.alessandro@gmail.com>
2023-05-12 16:09:33 +0200

    Fix

--- flake.nix
+++ flake.nix
@@ -5,10 +5,8 @@
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixgl = {
-      url = "github:guibou/nixGL";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixgl.url = "github:guibou/nixGL";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
