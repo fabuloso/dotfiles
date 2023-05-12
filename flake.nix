42dc23b7ac326cd12d4f9fa7cc57fa2bf5e8ccad
Alessandro <dalfovo.alessandro@gmail.com>
2023-05-12 17:12:27 +0200

    Change

--- flake.nix
+++ flake.nix
@@ -5,8 +5,10 @@
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixgl.url = "github:guibou/nixGL";
-
+    nixgl = {
+      url = "github:guibou/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,9 +17,9 @@
 
   outputs = { self, nixpkgs, nixpkgs-unstable, nixgl, home-manager, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system} // {
-        inherit system;
+      # pkgs = nixpkgs.legacyPackages.${system} // {
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
         config = { allowUnfree = true; };
         overlays = [ nixgl.overlay ];
       };
